33955b5958cd9e8b569d4ca7ec435eb9502df5c3
haris <harisiftikhar109@gmail.com>
2023-09-21 13:42:30 +0500

    added google auth

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,18 +1,55 @@
+// This is a Next-Auth Route
 // We will be using the nextJS API routes
 
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
+import User from "@models/user";
+import { connectToDB } from "@utils/database";
+
 const handler = NextAuth({
   providers: [
     GoogleProvider({
-      clientId: "",
-      clientSecret: "",
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
 
-  async session({ session }) {},
-  async signIn({ profile }) {},
+  callbacks: {
+    //  to get data anout an existing an running session
+    async session({ session }) {
+      const sessionUser = await User.findOne({
+        email: session.user.email,
+      });
+
+      // we are updating to making sure which user is currently online
+      session.user.id = sessionUser._id.toString();
+      return session;
+    },
+
+    async signIn({ profile }) {
+      try {
+        await connectToDB();
+
+        // check if a user already exists
+        const userExists = await User.findOne({ email: profile.email });
+
+        // if not, create a new user
+        if (!userExists) {
+          await User.create({
+            email: profile.email,
+            username: profile.name.replace(" ", "").toLowerCase(),
+            image: profile.picture,
+          });
+        }
+
+        return true;
+      } catch (error) {
+        console.log(error);
+        return false;
+      }
+    },
+  },
 });
 
 export { handler as GET, handler as POST };
